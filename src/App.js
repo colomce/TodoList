2ee6f48dee980a98500e086d91b6fd51be6b230a
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 21:38:28 +0800

    [AntDesign][Cedric] feature: Added navigation component and necessary changes in App.js and stylesheet

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import DoneListContainer from './containers/DoneListContainer';
 import { BrowserRouter, Route,Switch } from 'react-router-dom';
 import TodoListContainer from './containers/TodoListContainer';
 import logo from './images/leaf.png';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
       <header className="App-header">
         <span style={{display: 'inline-block' }}>
-          
           <h1 id="appName"><img src={logo} width="70" alt="logo" />TodoList</h1>
         </span>
 
-        <BrowserRouter>
-          <Switch>
-            <Route path="/done" component={DoneListContainer} />
-            <Route path="/" component={TodoListContainer} />
-          </Switch>
-        </BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route path="/done" component={DoneListContainer} />
+          <Route path="/" component={TodoListContainer} />
+        </Switch>
+
       </header>
+      </BrowserRouter>
     </div>
   );
 }
