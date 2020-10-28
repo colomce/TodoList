901440ac1b2b456fbe396bc20d9790248acc7f4a
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 20:52:23 +0800

    [AntDesign][Cedric] feature: added font and new logo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import DoneListContainer from './containers/DoneListContainer';
 import { BrowserRouter, Route,Switch } from 'react-router-dom';
 import TodoListContainer from './containers/TodoListContainer';
+import logo from './images/leaf.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <span style={{display: 'inline-block' }}>
+          
+          <h1 id="appName"><img src={logo} width="70"/>TodoList</h1>
+        </span>
+
         <BrowserRouter>
           <Switch>
             <Route path="/done" component={DoneListContainer} />
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
+import './fonts.css';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import reducers from './reducers';
