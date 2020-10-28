521bac790bbee5fd43f609ce2d7dcbaae4f2417b
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 19:29:12 +0800

    [AntDesign][Cedric] feature: Implemented spinner in TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,20 +3,32 @@ import TodoGeneratorContainer from '../containers/TodoGeneratorContainer';
 import TodoGroupContainer from '../containers/TodoGroupContainer';
 import {Link } from 'react-router-dom';
 import {getTodos} from '../apis/todo';
+import Spinner from './Spinner';
 
 class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {loading: true};
+  }
+  
   componentDidMount() {
+    this.setState({loading: true});
     getTodos().then(response => {
       this.props.getTodos(response.data);
+      this.setState({loading: false});
     });
   }
+
   render() {
     return (
       <div>
         <h1 id="appName">TodoList</h1>
         <Link to="/done" className="white link">done</Link>
         <TodoGeneratorContainer/>
-        <TodoGroupContainer/>
+        {
+          this.state.loading ? (<Spinner/>) : (<TodoGroupContainer/>)
+        }
       </div>
     );
   }
