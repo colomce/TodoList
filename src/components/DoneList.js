05330e2efd47fb448d57ce22e0ff4b77d15ce3cf
Cedric Don Coloma <colomce@oocl.com>
2020-10-28 19:42:47 +0800

    [AntDesign][Cedric] feature: Added Spinner in DoneList

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import TodoItemView from './TodoItemView';
 import { Link } from 'react-router-dom';
 import { getTodos } from '../apis/todo';
+import Spinner from './Spinner';
 
 class DoneList extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {loading: true};
+    }
 
     componentDidMount() {
+        this.setState({loading: true});
         getTodos().then(response => {
-          this.props.getTodos(response.data);
+            this.props.getTodos(response.data);
+            this.setState({loading: false});
         });
-      }
+    }
 
     render() {
         const doneTodo = this.props.todos
@@ -19,7 +27,8 @@ class DoneList extends Component {
             <div>
                 <h1 className="white">Done</h1>
                 <Link to="/" className="white link">todo</Link>
-                {doneTodo}
+                <div></div>
+                {this.state.loading ? (<Spinner/>) : doneTodo}
             </div>
         );
     }
