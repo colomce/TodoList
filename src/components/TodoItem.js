99942179c4bf217e898d51bd8ce20b62bee9e9c5
Cedric Don Coloma <colomce@oocl.com>
2020-10-29 21:56:17 +0800

    [SpringTodo][Cedric-Charlie] feature: Changed X icon to antd CloseCircleOutlined

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {deleteTodo, getTodos, updateTodo } from '../apis/todo';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {CheckSquareOutlined, RadiusBottomleftOutlined} from '@ant-design/icons';
+import {CheckSquareOutlined, RadiusBottomleftOutlined, CloseCircleOutlined} from '@ant-design/icons';
 
 class TodoItem extends Component {
 
@@ -71,7 +71,7 @@ class TodoItem extends Component {
         this.setState({editing: false});
         return;
       }
-      
+
       updateTodo(id,  {id, text: this.state.todoEditValue , done: done}).then(response => {
         getTodos().then(response => {
           this.props.getTodos(response.data);
@@ -125,7 +125,7 @@ class TodoItem extends Component {
               }
             </span>
           </div>
-          <span className="col-8" onClick={() => onDelete(this.props.todo.id)}><span id="deleteIcon">X</span></span>
+          <span className="col-8" onClick={() => onDelete(this.props.todo.id)}><span id="deleteIcon"><CloseCircleOutlined /></span></span>
         </div>
       </div>
     );
